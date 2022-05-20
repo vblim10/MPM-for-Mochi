d67fe7315957334952b55425bd6dceaca283da63
vblim10 <vblim10@ucla.edu>
2022-05-19 18:40:03 -0700

    Add files via upload

--- Code/1. Pounding Mochi/mochi_pound_3d.m
+++ Code/1. Pounding Mochi/mochi_pound_3d.m
@@ -277,7 +277,7 @@ end
 time = toc;
 
 % "Actual" Energy over time 
-EvT(k+1,:) = [t,RealEngy(Jk,Bk,Uk,w,Phi_k,Qconst)]; % [t,E,Q,K,Q_El,Q_N]
+EvT(k+1,:) = [t,RealEngy(Jk,Bk,Uk,w,Phi_k,Qconst)]; % [t,E,Q,K,Q_El,Q_N,G]
 
 % ++++++++++++++++++++++++++++ PLOTS +++++++++++++++++++++++++++++++++++++
 
